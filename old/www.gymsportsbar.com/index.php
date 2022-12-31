616d6dc032afcd73844a6c448998853e77d1d5a2
Jonathan Cottrell <cottrell.jonathan@gmail.com>
2022-12-31 15:06:40 -0800

    Move stuff

---
+++ old/www.gymsportsbar.com/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html class="sIFR-active" xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
+<head>
+<title>GYM Sportsbar - America's Most Popular Gay Sports Bar</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="Description" content="New York City's ORIGINAL Gay Sports Bar" />
+<meta name="Keywords" content="GYM Sportsbar, Gay Sports Bar, New York" />
+<meta name="Distribution" content="global" />
+<meta name="robots" content="index,follow" />
+<meta name="revisit-after" content="30 Days" />
+<link rel="icon" type="image/png" href="http://www.gymsportsbar.com/favicon.png">
+<link rel="stylesheet" href="squeezebox/SqueezeBox.css" type="text/css" media="screen">
+<link href="gymstyle.css" rel="stylesheet" type="text/css">
+<script language="JavaScript" type="text/JavaScript">
+<!--
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+
+
+//-->
+</script>
+
+
+
+
+</head>
+
+
+<body bgcolor="#000000" leftmargin="0" topmargin="175" marginwidth="0" marginheight="140">
+		<table width="100%" align="center" cellpadding="0" cellspacing="0" border="0">
+
+			<tr height="1">
+				<td colspan="3" bgcolor="white" height="1" align="center"></td>
+			</tr>
+            
+			<tr height="1" nowrap="nowrap">
+				<td bgcolor="#000000" width="350" align="center"><nobr>
+                <img src="images/main_gymlogo.gif" /><br />
+                <a href="nyhome.html" target="_self" onMouseOver="MM_swapImage('nyhomepage','','images/main_ny_b.gif',1)" onMouseOut="MM_swapImgRestore()"><img src="images/main_ny_a.gif" alt="" width="350" height="74" border="0" name="nyhomepage"></a><br />
+				<a href="https://www.facebook.com/GymSportsbarandGrill" target="_self" onMouseOver="MM_swapImage('lahomepage','','images/main_la_b.gif',1)" onMouseOut="MM_swapImgRestore()"><img src="images/main_la_a.gif" alt="" width="350" height="74" border="0" name="lahomepage"></a><br />
+                <a href="http://www.facebook.com/gymsportsbarfortlauderdale" target="_blank" onMouseOver="MM_swapImage('flhomepage','','images/main_fl_b.gif',1)" onMouseOut="MM_swapImgRestore()"><img src="images/main_fl_a.gif" alt="" width="350" height="73" border="0" name="flhomepage"></a>
+                <!--
+                <a href="nyhome.html"><img src="images/main_gym_a.gif" alt="" width="214" height="342" border="0" /><a href="nyhome.html"><img src="images/main_gym_b.gif" alt="" width="262" height="342" border="0"></a>
+                -->
+                <td width="800">
+<iframe src="https://player.vimeo.com/video/127615957?autoplay=1&title=0&byline=0&portrait=0" width="800" height="450" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                </td>
+                <td>
+				
+				</td>
+			</tr>
+			<tr height="1">
+				<td colspan="3" bgcolor="white" height="1" align="center"></td>
+			</tr>
+			
+		</table><br><br><br>
+		 <div align="center"><a href="https://216.131.125.39:2096" target="_blank"><font color="ffffff">..::..</font></a></div>
+	</body>
+</html>
